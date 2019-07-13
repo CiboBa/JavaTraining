2669588d0c1f289d66e56379acb5b4b6359bfc9b
Bartek Cib <bciborowski.pl@gmail.com>
2019-07-13 13:06:34 +0200

    Exception handling commited.

---
+++ src/main/java/exceptionFiles/NegativePaymentException.java
@@ -0,0 +1,16 @@
+package exceptionFiles;
+
+public class NegativePaymentException extends Exception {
+
+    double payment;
+
+    public NegativePaymentException(double payment) {
+        this.payment = payment;
+        System.out.println("ERROR: negative payment");
+    }
+
+    @Override
+    public String toString() {
+        return "ERROR: Cannot take negative payment: " + payment;
+    }
+}
---
+++ src/main/java/exceptionFiles/PaymentsApp.java
@@ -0,0 +1,41 @@
+package exceptionFiles;
+
+import java.util.Scanner;
+
+public class PaymentsApp {
+
+    /*
+    Take a payment from a user
+    1. Ask the user for input
+    2. Get the amount and test the value
+    3. Handle exception appropriately
+    4. Print confirmation
+    */
+
+    public static void main(String[] args) {
+
+        double payment = 0;
+        boolean positivePayment = true;
+
+        do {
+            System.out.println("Enter the payment amount: ");
+
+            Scanner scanner = new Scanner(System.in);
+
+            try {
+                payment = scanner.nextDouble();
+                if (payment < 0) {
+                    throw new NegativePaymentException(payment);
+                } else {
+                    positivePayment = true;
+                }
+            } catch (NegativePaymentException e) {
+                System.out.println(e.toString());
+                System.out.println("Please try again");
+                positivePayment = false;
+
+            }
+        } while (!positivePayment);
+        System.out.println("Thank you for your payment $" + payment);
+    }
+}
